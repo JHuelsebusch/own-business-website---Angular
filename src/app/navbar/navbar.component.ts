8675803e456b69c20bcbb5902876773dc8b89d55
JHuelsebusch <jhuelsebusch@gmx.de>
2023-05-04 14:13:25 +0200

    added burgermenu

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -6,5 +6,32 @@ import { Component } from '@angular/core';
   styleUrls: ['./navbar.component.scss']
 })
 export class NavbarComponent {
+isMenuOpen = false;
 
+menuLinks = [{
+  href: "#home",
+  name: "Home",
+},{
+  href: "#service",
+  name: "Service"
+},{
+  href: "#labs",
+  name: "Labore"
+},{
+  href: "#about",
+  name: "About"
+},{
+  href: "#partner",
+  name: "Partner"
+}]
+
+toggleMenu(): void {
+  this.isMenuOpen = !this.isMenuOpen;
 }
+closeMenu() {
+  this.toggleMenu();
+  const checkbox = document.getElementById('check',) as HTMLInputElement | null;
+  if(checkbox != null){
+    checkbox.checked = false;
+  }
+}}
