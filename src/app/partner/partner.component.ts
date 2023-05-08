b58f339e10d4b9b296e563a1a31538cd48bb03b7
JHuelsebusch <jhuelsebusch@gmx.de>
2023-05-08 17:33:04 +0200

    updated partner section

--- src/app/partner/partner.component.ts
+++ src/app/partner/partner.component.ts
@@ -7,4 +7,20 @@ import { Component } from '@angular/core';
 })
 export class PartnerComponent {
 
+  partner = [{
+    name: "Albert Littau GmbH",
+    description: "FBA Coach & Amazon Händler",
+    link: "https://albertlittau.de/",
+    img: "albert-littau.jpg"
+  },{
+name: "Kanzlei SKULD",
+    description: "Steuerberater & Rechtsanwälte für Online Händler & Heilberufe",
+    link: "https://kanzlei-skuld.de/",
+    img: "skuld.jpg"
+  }, {
+    name: "BBS Lemgo",
+    description: "Büro- und Buchhaltungs-Service",
+    link: "https://bbs-lemgo.de/",
+    img: "bbs-logo.jpg"
+  }]
 }
